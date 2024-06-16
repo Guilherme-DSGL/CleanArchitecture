61aaeb92b084f5e2ab250c61a6f3d5302eef9f5a
Guilherme <guilermeleitaoo@gmail.com>
2024-06-16 11:29:17 -0300

    feat: add domain entities

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Domain.Entities;
+
+namespace CleanArchitecture.Domain.Entities;
+
+public sealed class User : BaseEntity
+{
+    public string? Email { get; set; }
+    public string? Name { get; set; }
+}
