cb5eb36415127b46da0775a17d5b793a8cda9458
Guilherme <guilermeleitaoo@gmail.com>
2024-06-18 19:44:20 -0300

    feat: add create user use case

---
+++ Application/UseCases/CreateUser/CreateUserValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace CleanArchitecture.Application.UseCases.CreateUser;
+
+public sealed class CreateUserValidator : AbstractValidator<CreateUserRequest>
+{
+    public CreateUserValidator()
+    {
+        RuleFor(user => user.Name).NotEmpty().MinimumLength(3).MaximumLength(50);
+        RuleFor(user => user.Email).NotEmpty().MaximumLength(50).EmailAddress();
+    }
+}
